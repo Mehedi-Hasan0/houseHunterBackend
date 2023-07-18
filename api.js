efc00ac27bd673fb506df615deb5148b57cd2caf
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-07-18 07:49:55 +0600

    Feat: mongoose setup, and user schema is created

--- api.js
+++ api.js
@@ -1,4 +1,6 @@
+require("dotenv").config()
 const express = require("express");
+const mongoose = require("mongoose")
 const cors = require("cors");
 
 const app = express();
@@ -10,6 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 
 const port = 5000;
 
-app.listen(port, () => {
-    console.log("Server is running on port", port)
-})
+async function main() {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.in81gjk.mongodb.net/house-hunter-db`)
+    try {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+main();

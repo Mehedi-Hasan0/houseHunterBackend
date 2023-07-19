cf0878ac2b87bd6844fbb44804bb42ff37271820
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-07-19 08:45:55 +0600

    Feat: house schema created

---
+++ models/house.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const houseScema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    address: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    bedrooms: {
+        type: String
+    },
+    bathrooms: {
+        trype: String
+    },
+    roomSize: {
+        type: String
+    },
+    houseImage: {
+        type: String
+    },
+    availablityDate: {
+        type: String
+    },
+    rentPerMonth: {
+        type: String,
+    },
+    phoneNumber: {
+        type: String,
+    },
+    discription: {
+        type: String
+    }
+}, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
+
+const Houses = mongoose.model("House", houseScema, "housesList");
+
+module.exports = Houses;
